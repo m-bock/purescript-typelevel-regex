1c91b9f76bba849ff0c6bdf85bd8a7d9647f7364
Michael Bock <no-day@posteo.net>
2023-09-25 22:02:16 +0200

    Change docs

--- demo/src/ReadmeDemo.purs
+++ demo/src/ReadmeDemo.purs
@@ -5,6 +5,7 @@ Apply regular expressions to type level strings.
 
 ## Features
 
+- Subset of JS regexes
 - Nice error messages
 - No runtime overhead
 
@@ -29,6 +30,8 @@ it reflects the type level input string if the regex matches.
 type RegexURL =
   "^(ftp|https?)://[a-z][a-z0-9]*\\.(app|com|org)(/[a-z]+)*(\\?([a-z]+))?$"
 
+-- The following only compiles if the type level matches the above regex:
+
 sample1 :: String
 sample1 = Regex.guard @RegexURL @"http://hello.com/path/to?query"
 
@@ -48,23 +51,14 @@ sample4 = Regex.guard @RegexURL @"ftp://hello.org/path/to/home"
 |                             |                          |
 | --------------------------- | ------------------------ |
 | Character literals          | `a`, `b`, `c`, ...       |
-|                             |                          |
 | Wildcards                   | `.`                      |
-|                             |                          |
 | Match Start/End             | `^`, "$"                 |
-|                             |                          |
 | Groups                      | `(abc)`, `(hello)`, ...  |
-|                             |                          |
 | Alternatives                | `a|b|c`, `(foo|bar)`     |
-|                             |                          |
 | Match Many                  | `(foo)*`                 |
-|                             |                          |
 | Match Some                  | `(foo)+`                 |
-|                             |                          |
 | Match Maybe                 | `(foo)?`                 |
-|                             |                          |
 | Character Classes           | `[abc]`, [a-z0-9_]       |
-|                             |                          |
 | Negative Character Classes  | `[^abc]`, [^a-z0-9_]     |
 
 
